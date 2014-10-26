b5954fd20875bcd70d2ec6a9314c5e91a3b5fffc
PaigeNT <paigenthompson@gmail.com>
2014-10-26 15:28:17 -0400

    editing: main

--- rollme.c
+++ rollme.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "d.h"
 
-int main()
-{
+/* LOCAL CONSTANTS */
+int r; //initial seed
+
+/* LOCAL FUNCTIONS */
+int d (int,int);
+
+
+/* MAIN */
+int main() 
+{ 
     srand(time(NULL));
     int r = rand();
     printf("%d\n", r);    
 
     printf("%s\n", "roll me");
     return 0;
+}
+
+/* FUNCTION DEFINITIONS */
+
+/* ROLL DICE
+    @todo: improve seeding */
+
+int d(int sides)
+{
+    return sides;
 }
