774744c4cd36307a2024f762565ceec9e0f31cf3
PaigeNT <paigenthompson@gmail.com>
2014-10-26 14:49:02 -0400

    new function: prints a random number

--- rollme.c
+++ rollme.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "d.h"
 
 int main()
 {
+    srand(time(NULL));
+    int r = rand();
+    printf("%d\n", r);    
+
     printf("%s\n", "roll me");
     return 0;
 }
