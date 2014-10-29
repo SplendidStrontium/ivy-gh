af30e1c2fad825a7d90efb230fd6c7faca9c422a
PaigeNT <paigenthompson@gmail.com>
2014-10-29 18:36:04 -0400

    todo: c++ style io to interact with user

--- rollme.cpp
+++ rollme.cpp
@@ -1,10 +1,14 @@
+#include <iostream>
+#include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+using namespace std;
 
 /* LOCAL CONSTANTS */
 int rndseed = 0;
+stringstream playerinput;
 
 /* LOCAL FUNCTIONS */
 int rolld(int sides);
@@ -21,6 +25,9 @@ int main()
 
     int firstroll = rolld(4);
     printf("%d\n", firstroll);
+
+    cout << "enter d4 or d20 to roll a die";
+    cout << "\n";
 
 return 0;
 }
