9c67fb00ccb4ac18b8ed48aaa0270ad65609e901
PaigeNT <paigenthompson@gmail.com>
2014-10-29 19:39:45 -0400

    add: location, i/o

--- rollme.cpp
+++ rollme.cpp
@@ -8,31 +8,51 @@ using namespace std;
 
 /* LOCAL CONSTANTS */
 int rndseed = 0;
+char WHEREAMI[] = "CHARACTERCREATION";
+string s;
 stringstream playerinput;
 
 /* LOCAL FUNCTIONS */
 int rolld(int sides);
-
+void printplayerlocation(char* location);
+void queryplayer();
 
 /* MAIN */
 int main() 
 { 
     srand(time(NULL));
     rndseed = rand();
-    printf("%d\n", rndseed);    
 
+    printplayerlocation(WHEREAMI); 
     printf("%s\n", "roll a d4");
 
     int firstroll = rolld(4);
     printf("%d\n", firstroll);
 
-    cout << "enter d4 or d20 to roll a die";
-    cout << "\n";
+    cout << "enter d4 or d20 to roll a die" << "\n";
+    queryplayer();
 
 return 0;
 }
 
 /* FUNCTION DEFINITIONS */
+
+/* PRINT OPTIONS TO PLAYER 
+    @TODO: make this vary 
+*/
+void queryplayer()
+{
+    printf("%s\n", "1: d4");
+    printf("%s\n", "2: d20");
+    cin >> s;
+    cout << s;
+}
+/* PRINT PLAYER LOCATION
+*/
+void printplayerlocation(char* location)
+{
+    printf("%s\n", location);
+}
 
 /* ROLL A #-SIDED DIE */
 int rolld(int sides)
